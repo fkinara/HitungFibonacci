2d0f15a0d50c9c85d732b1fb81511ebfdbf7a66c
WINDOWS 10 <WINDOWS 10@DESKTOP-38EJHUP>
2018-09-12 13:06:04 +0700

    Initial Commit

--- src/hitungfibonacci/HitungFibonacci.java
+++ src/hitungfibonacci/HitungFibonacci.java
@@ -5,8 +5,7 @@
  */
 package hitungfibonacci;
 
-import java.util.Scanner;
-import java.math.BigInteger;
+
 public class HitungFibonacci {
     private static void tampilJudul(String identitas) {
         System.out.println("Identitas : "+ identitas);
@@ -14,42 +13,11 @@ public class HitungFibonacci {
         System.out.println("1, 1, 2, 3, 4, 5, 8, 13, 21, ... dst.\n");
     }
     
-    
-    private static int tampilInput(){
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Bilangan ke-: ");
-        int n = scanner.nextInt();
-        
-        return n;
-    }
-    
-    private static BigInteger fibo(int n){
-        
-        BigInteger[] hasil = new BigInteger[n];
-        
-        hasil[0] = BigInteger.ONE;
-        hasil[1] = BigInteger.ONE;
-        
-        for (int i = 2; i < n; i++){
-            hasil[i] = hasil[i-1].add(hasil[i-2]);
-        }
-        
-        return hasil[n-1];
-    }
-    
-    private static void tampilHasil(int n, BigInteger hasil) {
-        System.out.println("Bilangan Fibonacci ke-"+n+" : "+ hasil);
-    }
-    
     public static void main(String[] args) {
         String identitas = "Kinara F / XR3 / 16";
         
         tampilJudul(identitas); 
         
-        int n = tampilInput();
-        BigInteger hasil = fibo(n);
-        tampilHasil(n, hasil);
     }
     
 }
