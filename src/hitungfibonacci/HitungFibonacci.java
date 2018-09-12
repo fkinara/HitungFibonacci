7da9e91a2ef7196905f4e36ebb30c141c3b52e69
WINDOWS 10 <WINDOWS 10@DESKTOP-38EJHUP>
2018-09-12 10:55:11 +0700

    Tambah Hitung Fibonacci

--- src/hitungfibonacci/HitungFibonacci.java
+++ src/hitungfibonacci/HitungFibonacci.java
@@ -6,6 +6,7 @@
 package hitungfibonacci;
 
 import java.util.Scanner;
+import java.math.BigInteger;
 public class HitungFibonacci {
     private static void tampilJudul(String identitas) {
         System.out.println("Identitas : "+ identitas);
@@ -23,12 +24,27 @@ public class HitungFibonacci {
         return n;
     }
     
+    private static BigInteger fibo(int n){
+        
+        BigInteger[] hasil = new BigInteger[n];
+        
+        hasil[0] = BigInteger.ONE;
+        hasil[1] = BigInteger.ONE;
+        
+        for (int i = 2; i < n; i++){
+            hasil[i] = hasil[i-1].add(hasil[i-2]);
+        }
+        
+        return hasil[n-1];
+    }
+    
     public static void main(String[] args) {
         String identitas = "Kinara F / XR3 / 16";
         
         tampilJudul(identitas); 
         
         int n = tampilInput();
+        BigInteger hasil = fibo(n);
     }
     
 }
