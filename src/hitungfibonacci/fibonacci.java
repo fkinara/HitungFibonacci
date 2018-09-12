6f81baa4eaba4d4bee0705cf3b381523e32348cb
WINDOWS 10 <WINDOWS 10@DESKTOP-38EJHUP>
2018-09-12 11:07:32 +0700

    Tambah Tampil Hasil

--- src/hitungfibonacci/fibonacci.java
+++ src/hitungfibonacci/fibonacci.java
@@ -39,11 +39,16 @@ public class fibonacci {
         return hasil[n-1];
     }
     
+    private static void tampilHasil(int n, BigInteger hasil) {
+        System.out.println("Bilangan Fibonacci ke-"+n+" : "+ hasil);
+    }
+    
     public static void main(String[] args) {
         String identitas = "Kinara F / XR3 / 16";
         
         tampilJudul(identitas); 
         int n = tampilInput();
         BigInteger hasil = fibo(n);
+        tampilHasil(n, hasil);
     }
 }
